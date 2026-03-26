8bc05817e47a8be2090c6fd0740b65311a113db2
Daniel <est.daniela.fuentesb@unimilitar.edu.co>
2026-03-26 10:05:02 -0500

    Se crea el metodo agregar y hay un error.

--- taller21260326/src/main/java/ListaEnlazada.java
+++ taller21260326/src/main/java/ListaEnlazada.java
@@ -4,4 +4,14 @@ public class ListaEnlazada {
     public ListaEnlazada (){
         cabeza = null;
     }
+    public void Agregar(int dato){
+        Nodo nuevo = new Nodo(dato);
+        if(cabeza==null){
+            cabeza=nuevo;
+        } else {
+            Nodo actual = cabeza;
+            while(actual.sig !=null)
+        }
+        
+    }
 }
